ba84459df9f38ced0f67044feb1d76c0d0f2175c
Aluno <Aluno@LAB0620>
2020-03-06 22:02:48 -0300

    Projeto Atualizado

---
+++ FANFIGURE/src/main/java/com/Modelos/Produto.java
@@ -0,0 +1,34 @@
+package com.Modelos;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	long id;
+	String nome;
+	String material;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getMaterial() {
+		return material;
+	}
+	public void setMaterial(String material) {
+		this.material = material;
+	}
+
+}
